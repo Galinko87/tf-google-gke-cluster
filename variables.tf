3e96c9198fb40cc52402c6d872efaecc4459efa4
Galinko87 <galyna.prykhodchenko@gmail.cm>
2024-05-09 22:37:18 +0200

    modified main and variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "GOOGLE_PROJECT" {
   type        = string
-  description = "GCP project name"
+  description = "GCP project to use"
 }
 
 variable "GOOGLE_REGION" {
@@ -9,26 +9,7 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type"
-}
-
 variable "GKE_NUM_NODES" {
   type        = number
-  default     = 2
-  description = "GKE nodes number"
-}
-
-variable "GKE_CLUSTER_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE cluster name"
-}
-
-variable "GKE_POOL_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE pool name"
+  description = "Number of nodes"
 }
